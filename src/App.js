d1a7335b28142c4f4118be7eb7501b59ca55c5c5
Laysa Souza <karollinylaysa@gmail.com>
2022-06-05 21:26:01 +0100

    Class App component converted to a Function Component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,40 @@
 import './App.css';
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {CardList} from "./components/card-list/CardList";
 import {SearchBox} from "./components/search-box/SearchBox";
 
-class App extends React.Component {
-    constructor() {
-        super(undefined);
+export default function App() {
 
-        this.state = {
-            monsters: [],
-            searchField: ''
-        };
+    const [searchField, setSearchField] = useState('');
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setfilteredMonsters] = useState(monsters);
 
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState(
-                () => {
-                    return {monsters: users}
-                }
-            ));
-    }
-
-    handleChange = (event) => {
-        const searchField = event.target.value.toLowerCase();
-        this.setState(() => {
-            return {searchField}
-        })
-
-    }
-
-    render() {
-        const {monsters, searchField} = this.state;
-        const {handleChange} = this;
-
-        const filteredMonsters = monsters.filter((monster) => {
+    useEffect(() => {
+        const newfilteredMonsters = monsters.filter((monster) => {
             return monster.name.toLowerCase().includes(searchField)
         });
+        setfilteredMonsters(newfilteredMonsters);
+    }, [monsters, searchField])
 
-        return (
-            <div className="App">
-                <SearchBox
-                    placeholder={'Search monsters'}
-                    handleChange={handleChange}
-                />
-                <CardList monsters = {filteredMonsters}/>
-            </div>
-        )
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => setMonsters(users));
+    }, []);
+
+    function handleChange(event) {
+        const searchFieldString = event.target.value.toLowerCase();
+        setSearchField(searchFieldString);
     }
-}
 
-export default App;
+
+    return (
+        <div className="App">
+            <SearchBox
+                placeholder={'Search monsters'}
+                handleChange={handleChange}
+            />
+            <CardList monsters={filteredMonsters}/>
+        </div>
+    )
+}
