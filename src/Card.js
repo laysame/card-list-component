b81f24f8181de84760ccb4dc80fb5e5d04845f2b
Laysa Souza <karollinylaysa@gmail.com>
2022-06-05 18:55:53 +0100

    Refactored Card class component

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,16 @@ import React from "react";
 import './Card.css';
 
 
-export const Card = props => {
-    return (
-        <div className="Card">
-            <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt={'monster'} />
-            <h1 key={props.id}>{props.monster.name}</h1>
-        </div>
-    )
+export class Card extends React.Component {
+
+    render() {
+
+        const {monster} = this.props;
+        return (
+            <div className="Card">
+                <img src={`https://robohash.org/${monster.id}?set=set2`} alt={'monster'}/>
+                <h1 key={monster.id}>{monster.name}</h1>
+            </div>
+        )
+    }
 }
